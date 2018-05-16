ecfc3523bed3cb76a93b1025e7e69161a0b8643a
Morgan <morganebridges@gmail.com>
2018-05-15 22:02:01 -0500

    adding model for tag

--- src/main/java/com/bridges/model/blog/Tag.java
+++ src/main/java/com/bridges/model/blog/Tag.java
@@ -1,5 +1,25 @@
 package com.bridges.model.blog;
 
+import com.sun.istack.internal.Nullable;
+import org.springframework.data.annotation.Id;
+
+import java.util.TreeMap;
+
 public class Tag {
+    @Id
+    @Nullable
+    private long id;
+
+    private String name;
+
+    private TreeMap associations;
+
+    public Tag(String name){
+        this.name = name;
+    }
+    public Tag(String name, Long id){
+        this.name = name;
+        this.id = id;
+    }
 
 }
